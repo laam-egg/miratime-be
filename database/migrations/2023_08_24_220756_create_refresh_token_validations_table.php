ac2d3691bdfab1f0227cf50a820fb35bf8b69829
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-08-30 00:16:53 +0700

    Update migration for refresh_token_validations table

--- database/migrations/2023_08_24_220756_create_refresh_token_validations_table.php
+++ database/migrations/2023_08_24_220756_create_refresh_token_validations_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('refresh_token_validations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->index(['user_id']);
         });
     }
 
@@ -22,10 +23,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('refresh_token_validations', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            // $table->dropIndex(['user_id']);
-        });
-        Schema::dropIfExists('refresh_token_validations');
+        if (Schema::hasTable('refresh_token_validations')) {
+            Schema::table('refresh_token_validations', function (Blueprint $table) {
+                $table->dropForeign(['user_id']);
+                $table->dropIndex(['user_id']);
+                $table->drop();
+            });
+        }
     }
 };
